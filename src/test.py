a365807a34157a00c4596176ed6220afcf8e1c9a
dafubj <zyshao@tom.com>
2020-05-07 13:47:07 +0800

    2020-05-07 14:00

--- src/test.py
+++ src/test.py
@@ -1,7 +1,6 @@
-from src import network2
+from src import network
 from src import mnist_loader
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
-net.large_weight_initializer()
-net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data,monitor_evaluation_accuracy=True)
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3)
